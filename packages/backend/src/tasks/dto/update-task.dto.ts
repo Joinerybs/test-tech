e3aa874bdfb49fb1b26eeff1453ffb6688f7a8b4
Quentin Riou <queriou@gmail.com>
2026-01-26 00:34:11 +0100

    Commentaires Schéma de validation pour modification de tâche

--- packages/backend/src/tasks/dto/update-task.dto.ts
+++ packages/backend/src/tasks/dto/update-task.dto.ts
@@ -1,5 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTaskDto } from './create-task.dto';
 
-// PartialType va reprendre toutes les règles de CreateTaskDto mais les rendre optionnelles (grace à PartialType)
+/**
+ * DTO pour la mise à jour d'une tâche
+ * Utilise PartialType pour rendre tous les champs de CreateTaskDto optionnels
+ * en conservant les règles de validation que la création d'une tâche
+ */
 export class UpdateTaskDto extends PartialType(CreateTaskDto) {}
